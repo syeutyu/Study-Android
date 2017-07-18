a69da569f9bac6b24de6950a80c2b23707d8d153
syeuty <syeuty@naver.com>
2017-07-18 23:44:56 +0900

    Calender

--- ets/CalendarExample/app/src/main/java/com/example/user/calendarexample/MainActivity.java
+++ ets/CalendarExample/app/src/main/java/com/example/user/calendarexample/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.user.calendarexample;
 
+import android.animation.PropertyValuesHolder;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
 
@@ -9,6 +15,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -56,7 +63,62 @@ public class MainActivity extends AppCompatActivity {
         setCalendar(Cal.get(Calendar.MONTH)+1);
         gridAdapter = new GridAdapter(getApplicationContext(),daylist);
         gridview.setAdapter(gridAdapter);
-
-
     }
+
+    private void setCalendar(int month){
+        Cal.set(Calendar.MONTH,month-1);
+
+        for(int i=0;i<Cal.getActualMaximum(Calendar.DAY_OF_MONTH);i++){
+            daylist.add(""+(i+1));
+        }
+    }
+
+
+}
+
+class GridAdapter extends BaseAdapter{
+    private  List<String> list;
+    private final LayoutInflater inflater;
+
+    GridAdapter(Context context,List<String> list) {
+        this.list = list;
+        this.inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder = null;
+        if(view == null){
+            view = inflater.inflate(R.layout.activity_calendar,viewGroup,false);
+
+            holder = new ViewHolder();
+            holder.tvItemGridView  = (TextView)view.findViewById(R.id.item);
+            view.setTag(holder);
+        }else{
+            holder = (ViewHolder)view.getTag();
+        }
+        holder.tvItemGridView.setText(""+getItem(i));
+
+            Cal = Calendar.getInstance();
+    }
+}
+
+class ViewHolder extends PropertyValuesHolder{
+
 }
