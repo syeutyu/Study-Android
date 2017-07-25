6226620c5e3d3e8d965746f8574e67e6c90caa11
syeuty <syeuty@naver.com>
2017-07-25 22:39:21 +0900

    삭제시 오류 수정

--- ets/Junier/app/src/main/java/com/example/user/junier/Day.java
+++ ets/Junier/app/src/main/java/com/example/user/junier/Day.java
@@ -1,8 +1,12 @@
 package com.example.user.junier;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,16 +14,16 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class Day extends AppCompatActivity {
-    public int count;
     String daydata;
+    public static String num;
     public ArrayList<String> arr = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day);
-
-
+        final Context context = this;
+        Button btn = (Button) findViewById(R.id.save);
         CalendarView cal = (CalendarView) findViewById(R.id.cal);
         final TextView day = (TextView) findViewById(R.id.datebelow);
 
@@ -29,13 +33,13 @@ public class Day extends AppCompatActivity {
 
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                try {
+                    String data = null;
+                    daydata = null;
+                    Boolean check = true;
 
-                String data = null;
-                daydata = null;
-                Boolean check = true;
-                if (count < 7) {
 
-                    String num = year + "," + (month + 1) + "," + dayOfMonth;
+                    num = year + "," + (month + 1) + "," + dayOfMonth;
                     //Log.v("텍스트 중앙",android:layout_centerInParent="true")
 
                     for (int i = 0; i < arr.size(); i++) {
@@ -49,48 +53,57 @@ public class Day extends AppCompatActivity {
                         }
 
                     }
-                    Log.v("check 값 : ", String.valueOf(check));
 
-                    if (check == true) {
+                    if (arr.size() < 7) {
 
-                        arr.add(num);
-                        Log.v("data 날짜", String.valueOf(arr.size()));
+                        Log.v("check 값 : ", String.valueOf(check));
 
-                        Toast.makeText(Day.this, num + "이 저장되었습니다. ", Toast.LENGTH_SHORT).show();
+                        if (check == true) {
 
-                        for (int i = 0; i < arr.size(); i++) {
+                            arr.add(num);
+                            Log.v("data 날짜", String.valueOf(arr.size()));
 
-                            for (int j = 0; j <= i; j++) {
-                                if (arr.get(j) != null)
-                                    data += arr.get(j) + " ";
-                            }
-                            data += "&";
-                        }
-                        Log.v("data 값 : ", data);
+                            Toast.makeText(Day.this, num + "이 저장되었습니다. ", Toast.LENGTH_SHORT).show();
 
-                        String[] ch = data.split("&");
+                            for (int i = 0; i < arr.size(); i++) {
 
-                        for (int i = 1; i < (ch.length + 1); i++) {
-
-                            if (ch[i - 1].contains("null")) {
-
-                                String[] ch2;
-                                ch2 = ch[i - 1].split("null");
-                                daydata = ch2[1];
-
-                            } else {
-                                if (i == 7) {
-                                    Log.v("오류 ", ch[i - 1]);
+                                for (int j = 0; j <= i; j++) {
+                                    if (arr.get(j) != null)
+                                        data += arr.get(j) + " ";
                                 }
-                                daydata = ch[i - 1];
+                                data += "&";
                             }
+                            Log.v("data 값 : ", data);
+
+                            String[] ch = data.split("&");
+
+                            for (int i = 1; i < (ch.length + 1); i++) {
+
+                                if (ch[i - 1].contains("null")) {
+
+                                    String[] ch2;
+                                    ch2 = ch[i - 1].split("null");
+                                    daydata = ch2[1];
+
+                                } else {
+                                    if (i == 7) {
+                                        Log.v("오류 ", ch[i - 1]);
+                                    }
+                                    daydata = ch[i - 1];
+                                }
+                            }
+
+
+                            day.setText(TextViewHelper.shrinkWithWordUnit(day, daydata, 100));
+
                         }
+                    } else {
 
+                        Toast.makeText(getApplicationContext(), "입력가능한 일수를 초과했습니다.", Toast.LENGTH_SHORT).show();
 
-                        count++;
-                        day.setText(TextViewHelper.shrinkWithWordUnit(day, daydata, 100));
+                    }
 
-                    } else if (check == false) {
+                    if (check == false) {
 
                         Toast.makeText(getApplicationContext(), "해당일을 취소합니다.", Toast.LENGTH_SHORT).show();
                         for (int i = 0; i < arr.size(); i++) {
@@ -128,7 +141,6 @@ public class Day extends AppCompatActivity {
                                     daydata = ch[i - 1];
                                 }
                             }
-                            count--;
                             day.setText(TextViewHelper.shrinkWithWordUnit(day, daydata, 100));
 
                         } else {
@@ -137,13 +149,24 @@ public class Day extends AppCompatActivity {
                         }
 
                     }
-                } else {
 
-                    Toast.makeText(getApplicationContext(), "입력가능한 일수를 초과했습니다.", Toast.LENGTH_SHORT).show();
-
+                } catch (Exception e) {
+                    e.getMessage();
                 }
             }
 
+        });
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
         });
     }
 }
